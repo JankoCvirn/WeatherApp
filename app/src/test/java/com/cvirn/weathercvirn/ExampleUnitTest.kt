47db12b5f6e0336ac6481dcad036210276053e63
Janko Cvirn <jankocvirn@gmail.com>
2022-03-16 11:59:42 +0100

    -add libs

--- app/src/test/java/com/cvirn/weathercvirn/ExampleUnitTest.kt
+++ app/src/test/java/com/cvirn/weathercvirn/ExampleUnitTest.kt
@@ -1,8 +1,7 @@
 package com.cvirn.weathercvirn
 
-import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Test
 
 /**
  * Example local unit test, which will execute on the development machine (host).
